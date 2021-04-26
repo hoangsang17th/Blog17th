a1753ce90050b212e5dc777ece940c606f4ca7d5
Phạm Hoàng Sang <hoangsang17th@gmail.com>
2021-04-26 23:52:31 +0700

    Lần cuối'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # @posts = Post.all
-    @@per_page = 10
+    @@per_page = 3
     @page = params.fetch(:page,0).to_i
     @posts = Post.order(created_at: :desc).offset(@page * @@per_page).limit(@@per_page)
     
